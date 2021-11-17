b9faa0d60bb45cd3a07d7089a0e95c8a80c9c54b
sear93 <egor.rajewski@gmail.com>
2021-11-17 14:08:35 +0200

    changed API_URI env variable

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
-        API_URI: "https://wp-dev.space/education/egor/develop/api",
+        API_URI: "https://wp-dev.space/education/egor/develop/wp-json/api",
         MONGO_URI: "mongodb+srv://egor:5901478900@cluster0.4wtxn.mongodb.net/ROVKS?retryWrites=true&w=majority"
     },
     // i18n: {
