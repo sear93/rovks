224fb9dc879b67ecd10c8e1309e71ab75521c135
sear93 <egor.rajewski@gmail.com>
2021-11-17 13:04:52 +0200

    removed graphQL env

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     env: {
-        GRAPHQL_URI: "http://education-egor.local/graphql",
-        API_URI: "http://education-egor.local/wp-json/api",
+        API_URI: "https://wp-dev.space/education/egor/develop/api",
         MONGO_URI: "mongodb+srv://egor:5901478900@cluster0.4wtxn.mongodb.net/ROVKS?retryWrites=true&w=majority"
     },
     // i18n: {
