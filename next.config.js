b146934d864889818976c154cf76a2b3fe0bd4f4
sear93 <egor.rajewski@gmail.com>
2021-12-06 13:57:22 +0200

    changed config file

--- next.config.js
+++ next.config.js
@@ -1,16 +1,27 @@
-const {PHASE_DEVELOPMENT_SERVER} = require('next/constants')
+const {PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_SERVER} = require('next/constants')
 
-const prodUrl = "https://wp-dev.space/education/egor/develop/wp-json/api"
-const devUrl = "http://education-egor.local/wp-json/api"
+module.exports = (phase) => {
+    if (phase === PHASE_DEVELOPMENT_SERVER) {
+        return {
+            env: {
+                API_URI: "http://education-egor.local/wp-json/api",
+                MONGO_URI: "mongodb+srv://egor:5901478900@cluster0.4wtxn.mongodb.net/ROVKS?retryWrites=true&w=majority"
+            },
+            images: {
+                domains: ['wp-dev.space', 'education-egor.local'],
+            },
+        }
+    }
 
-module.exports = {
-    env: {
-        API_URI: PHASE_DEVELOPMENT_SERVER ? devUrl : prodUrl,
-        MONGO_URI: "mongodb+srv://egor:5901478900@cluster0.4wtxn.mongodb.net/ROVKS?retryWrites=true&w=majority"
-    },
-    images: {
-        domains: ['wp-dev.space', 'education-egor.local'],
-    },
+    return {
+        env: {
+            API_URI: "https://wp-dev.space/education/egor/develop/wp-json/api",
+            MONGO_URI: "mongodb+srv://egor:5901478900@cluster0.4wtxn.mongodb.net/ROVKS?retryWrites=true&w=majority"
+        },
+        images: {
+            domains: ['wp-dev.space', 'education-egor.local'],
+        },
+    }
     // i18n: {
     //     locales: ['en', 'fr', 'de', 'sv'],
     //     defaultLocale: 'en',
